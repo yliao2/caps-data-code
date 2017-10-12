cee2c428863db94cd0fc665256b1c9331c7c4ee9
Yang-Li Liao <30913475+yliao2@users.noreply.github.com>
2017-10-11 21:22:21 -0500

    Add files via upload

--- all_plot.R
+++ all_plot.R
@@ -101,14 +101,14 @@ level.results <- function(alpha)
   a.rank <- test$all %>% mutate(p = n/sum(n), pred.p = pred/sum(pred), ratio.p = p/pred.p) %>% arrange(rank)
   p.rank <- a.rank %>% arrange(pred.rank)
   n <- nrow(a.rank)
-  out <- matrix(0, length(alpha), 8)
+  out <- matrix(0, length(alpha), 10)
   for (i in 1:length(alpha))
   {
     if(alpha[i]>=1/n)
     {
       for (j in 1:n)
       {
-        if((j/n)<=alpha[i]){out[i, 2] <- j/n; out[i, 3] <- j; out[]}
+        if((j/n)<=alpha[i]){out[i, 2] <- j/n; out[i, 3] <- j}
         else{out[i, 2] <- out[i, 2]; out[i, 3] <- out[i, 3]}
       }
       out[i, 1] <- alpha[i]
@@ -117,11 +117,13 @@ level.results <- function(alpha)
       out[i, 6] <- cumsum(p.rank$pred.p)[out[i, 3]]
       out[i, 7] <- p.rank[out[i, 3],]$pred.p
       out[i, 8] <- sum(p.rank$pred.p[p.rank$pred.p>=out[i, 5]])
+      out[i, 9] <- sum(p.rank$pred.p>=out[i, 5])/n
+      out[i,10] <- sum(p.rank$pred.p>=out[i, 5])
     }
-    else{out[i, 1] <- alpha[i]; out[i, c(2, 3, 4, 6, 8)] <- 0; out[i, c(5, 7)] <- 1}
+    else{out[i, 1] <- alpha[i]; out[i, c(2, 3, 4, 6, 8, 9, 10)] <- 0; out[i, c(5, 7)] <- 1}
   }
   colnames(out) <- c("alpha", "pct.hs", "times", "cum.p", "threshold", 
-                     "cum.pred", "pred.threshold", "pct.pred.hs")
+                     "cum.pred", "pred.threshold", "thres.pred.p", "pct.pred.hs", "pred.times")
   list(out = out, a.rank = a.rank, p.rank = p.rank)
 }
 
@@ -145,68 +147,108 @@ legend("topright", c("obs.", "pred.", "id obs. HS", "id pred. HS"),
        col = c(1, 4, 2, 4), lty = c(1, 1, NA, NA), lwd = c(1, 2, NA, NA), pch = c(NA, NA, 1, 1))
 
 
-
 #sequence of alphas (alpha = seq(0, 1, length = 101))
 #====================================================
-
 #surveillance plot (level alpha vs. % events in HS)
 #--------------------------------------------------
 alpha <- seq(0, 1, length = 101)
 l <- level.results(alpha)
-plot(x = l$out[,c("alpha")], y = l$out[,c("cum.p")], type = "s", xlab = expression(alpha),
-     ylab = "% events in HS")
-lines(x = l$out[,c("alpha")], y = l$out[,c("cum.pred")], type = "s", col = 4)
+plot(x = l$out[,"alpha"], y = l$out[,"cum.p"], type = "s", 
+     xlab = expression(alpha), ylab = "% events in HS")
+lines(x = l$out[,"alpha"], y = l$out[,"cum.pred"], type = "s", col = 4)
 legend("right", c("obs.", "pred."), col = c(1, 4), lty = 1)
 
-#% HS above threshold (based on observations) vs. level alpha (obs. vs. pred.)
+
+
+#% HS above same threshold vs. level alpha (obs. vs. pred.) (modified 2017/10/9)
 #-----------------------------------------------------------------------------
-plot(x = l$out[,c("alpha")], y = l$out[,c("pct.hs")], type = "s", xlab = expression(alpha),
+plot(x = l$out[, "alpha"], y = l$out[, "pct.hs"], type = "s", xlab = expression(alpha),
      ylab = "% HS")
-lines(x = l$out[, c("alpha")], y = l$out[,c("pct.pred.hs")], type = "s", col = 4)
+lines(x = l$out[, "alpha"], y = l$out[, "pct.pred.hs"], type = "s", col = 4)
 abline(a = 0, b = 1)
 legend("right", c("obs.", "pred."), col = c(1, 4), lty = 1)
 
+plot(x = l$out[, "pct.hs"], y = l$out[, "pct.pred.hs"], type = "s", xlab = "obs. level",
+     ylab = "pred. level", col = 4)
+abline(a = 0, b = 1, col = "darkgrey")
 
 
-
-#similar KS results
-#------------------
-head(l$out)
-ks.results <- a %>% select(Logmile, p, pred.p) %>% 
-  mutate(s.p = cumsum(p), s.pred.p = cumsum(pred.p), d = abs(s.p-s.pred.p))
-sig.level <- cbind(alpha = c(0.1, 0.05, 0.025, 0.01, 0.005, 0.001),
-                   value = c(1.22, 1.36, 1.48, 1.63, 1.73, 1.95))
-bound <- sig.level[, 2]*sqrt((2*nrow(a)/(nrow(a)^2)))
-max(ks.results$d)
+#add: size of HS vs. threshold
+#-----------------------------
+plot(x = l$out[-1, "threshold"], y = l$out[-1, c("pct.hs")], type = "s", 
+     xlab = "threshold", ylab = "size of hotspot")
+lines(x = l$out[-1, "threshold"], y = l$out[-1, c("pct.pred.hs")], type = "s", col = 4)
+legend("right", c("obs.", "pred."), col = c(1, 4), lty = 1)
 
 
+#add: % matched segments
+#-------------------------
+t1 <- as.data.frame(l$out)
+t2 <- l$a.rank
+t3 <- l$p.rank
+m1 <- NULL
+for (i in 1:nrow(t1))
+{
+  m1 <- c(m1, sum(t2[1:t1$times[i],"Logmile"] %in% t3[1:t1$times[i],"Logmile"])/t1$times[i])
+}
+plot(x = t1$alpha, y = m1, xlab = expression(alpha), ylab = "% matched logmile/segments",
+     type = "s")
+abline(h = seq(0, 1, by = 0.2), v = seq(0, 1, by = 0.2), col = "lightgrey")
 
-#####alpha = % events above threshold
-#single.level <- function(v)
-#{
-#  windows()
-#  plot(a$Logmile, a$p, xlab = "Logmile", cex = 0, ylab = "p")
-#  segments(x0 = a$Logmile, y0 = a$p, y1 = 0)
-#  lines(x = a$Logmile, y = a$pred.p, col = 4, lwd = 2)
-#  
-#  points(l$a.rank[1:l$out[v,3],]$Logmile, l$a.rank[1:l$out[v,3],]$p, col = 2)
-#  abline(h = l$out[v,5], col = 2)
-#  text(x = max(l$a.rank$Logmile)-1, y = l$out[v,5], label = round(l$out[v,5], 4), pos = 3, col = 2)
-#  
-#  points(l$p.rank[1:l$out[v,7],]$Logmile, l$p.rank[1:l$out[v,7],]$pred.p, col = 4)
-#  abline(h = l$out[v,9], col = 4)
-#  text(x = max(l$p.rank$Logmile)-1, y = l$out[v,9], label = round(l$out[v,9], 4), pos = 3, col = 4)
-#}
-#single.level(30)
-#
-####sequence of alpha
-#colnames(l$out)
 
-#windows()
-#plot(x = l$out[,c("pct.hs")], y = l$out[,c("alpha")], type = "s", 
-#     xlab = "% hotspot", ylab = expression(alpha))
-#lines(x = l$out[,c("pre.pct.hs")], y = l$out[,c("alpha")], type = "s", col = 4)
-#
-#windows()
-#plot(x = l$out[,c("pct.hs")], y = l$out[,c("cum.p")], type = "s")
-#lines(x = l$out[,c("pre.pct.hs")], y = l$out[,c("cum.pred")], type = "s", col = 4)
+#add: relative distribution (ratio = obs/pred) in KS test comparing unif(0, 1)
+r <- s %>% select(alpha, pct.hs, pct.pred.hs) %>% mutate(r.pct.hs = pct.hs/pct.pred.hs)
+n <- length(r$r.pct.hs[is.finite(r$r.pct.hs)])
+r1 <- r$r.pct.hs/sum(r$r.pct.hs[is.finite(r$r.pct.hs)])
+r2 <- rep(1, n)/n
+plot(x = r$alpha, y = r$r.pct.hs, type = "s", xlab = expression(alpha), ylab = "ratio", col = 4)
+abline(h = 1, col = "lightgrey")
+plot(x = r$alpha[is.finite(r$r.pct.hs)], 
+     y = cumsum(r1[is.finite(r1)]), type = "s", xlab = expression(alpha), ylab = NA, col = 4)
+lines(x = r$alpha[is.finite(r$r.pct.hs)], y = cumsum(r2), type = "s")
+abline(v = seq(0, 1, by = 0.2), h = seq(0, 1, by = 0.2), col = "lightgrey")
+
+
+#quasi KS test (for Obs. vs. Pred.)
+#used for check model fit
+#nonparametric statistical inference 4ed, gibbons
+#------------------------------------------------
+s <- data.frame(l$out)
+
+#quasi KS test function
+ks.results <- function(s1, s2, alpha)
+{
+  n <- length(s1); m <- length(s2)
+  c.alpha <- sqrt(-0.5*log(alpha/2))
+  bound <- c.alpha*sqrt(1/n+1/m)
+  max.d <- max(abs(s1-s2))
+  p <- 2*exp(-2*max.d^2/(1/n+1/m))
+  if (max.d>=bound){out = paste("Reject H0. max(|s1-s2|) = ", round(max.d, 4), " >= ", 
+                                round(bound, 4), " at sig. level = ", alpha, sep = "")}
+  else {out = paste("Fail to reject H0. max(|s1-s2|) = ", round(max.d, 4), " < ", 
+                    round(bound, 4), " at sig. level = ", alpha, sep = "")}
+  list(out = out, max.d = max.d, p.value = p, bound = bound)
+}
+
+#quasi KS test for {surveillance plot (level alpha vs. % events in HS). line: 152}
+ks.results(s1 = s$cum.p, s2 = s$cum.pred, alpha = 0.05)
+ks.results(s1 = s$cum.p, s2 = s$cum.pred, alpha = 0.005)
+
+#quasi KS test for {size of HS vs. threshold. line: 176}
+ks.results(s1 = s$pct.hs, s2 = s$pct.pred.hs, alpha = 0.05)
+
+#chi-sqaured test & qks test (for Obs. vs. expected) (uniform)
+rel.chisq <- function(obs, pred, alpha)
+{
+  n <- length(obs)
+  d <- n*sum((obs-pred)^2/pred)
+  p <- pchisq(d, df = n-1, lower.tail = FALSE)
+  bound <- qchisq(1-alpha, df = n-1)
+  if(d>=bound){out = paste("Reject H0. chi-sq = ", round(d, 4), " >= ", 
+                           round(bound, 4), " at sig. level = ", alpha, sep = "")}
+  else{out = paste("Fail to reject H0. chi-sq = ", round(d, 4), " < ", 
+                   round(bound, 4), " at sig. level = ", alpha, sep = "")}
+  list(out = out, statistics = d, pvalue = p)
+}
+rel.chisq(obs = r1[is.finite(r1)], pred = r2, alpha = 0.05)
+ks.results(s1 = cumsum(r1[is.finite(r1)]), s2 = cumsum(r2), alpha = 0.05)
